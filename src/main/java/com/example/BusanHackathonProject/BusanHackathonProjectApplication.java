4a040bf0ad43034b8f4849028c11d03e79738c0f
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-02-15 14:13:10 +0900

    초기설정

---
+++ src/main/java/com/example/BusanHackathonProject/BusanHackathonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.BusanHackathonProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanHackathonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusanHackathonProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BusanHackathonProject/BusanHackathonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusanHackathonProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusanHackathonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
